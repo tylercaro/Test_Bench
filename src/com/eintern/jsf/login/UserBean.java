6dfe430682d6b10caf9fb7ea9068b8c14a7aec32
Tyler Caro <tylercaro@gmail.com>
2015-08-25 11:30:06 -0400

    Added JSF Validation

--- src/com/eintern/jsf/login/UserBean.java
+++ src/com/eintern/jsf/login/UserBean.java
@@ -1,51 +1,62 @@
 package com.eintern.jsf.login;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
 import com.eintern.model.User;
 
 @ManagedBean
 @SessionScoped
 public class UserBean {
-	private User user=new User();
-	
+	private User user = new User();
+
 	public User getUser() {
 		return user;
 	}
-
 
 	public void setUser(User user) {
 		this.user = user;
 	}
 
-
 	public void setUserdao(UserDao userdao) {
 		this.userdao = userdao;
 	}
 
-	@ManagedProperty(value="#{userDao}")
+	@ManagedProperty(value = "#{userDao}")
 	private UserDao userdao;
 
 	public UserDao getUserdao() {
 		return userdao;
 	}
 
-
 	public String login() {
 		String nextPage = "failure";
-		User userFromDb= userdao.getUser(user.getUsername());
-		if (user.getUsername().equals(userFromDb.getUsername())&& user.getPassword().equals(userFromDb.getPassword())) {
+		User userFromDb = userdao.getUser(user.getUsername());
+		if (user.getUsername().equals(userFromDb.getUsername())
+				&& user.getPassword().equals(userFromDb.getPassword())) {
 			nextPage = "success?faces-redirect=true";
 		}
 		return nextPage;
 	}
-	
-	public String register(){
-		String nextPage;
-		userdao.saveObject(user);
-		nextPage="success";
+
+	public String register() {
+		String nextPage="";
+		if (userdao.getUser(user.getUsername()) != null) {
+			//get the current instance of FacesContext that is associated with this request and add error msg
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("user already exists"));
+		}
+
+		else {
+		
+			userdao.saveObject(user);
+		}
+		//if no messages/errors exist, forward to next page
+		if(FacesContext.getCurrentInstance().getMessageList().isEmpty()){
+			nextPage = "success";
+		}
 		return nextPage;
 	}
 }
