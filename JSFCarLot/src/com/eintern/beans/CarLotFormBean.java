5615d03893a773b2ad1b785fcb91bf9baa8663f5
Tyler Caro <tylercaro@gmail.com>
2015-08-29 18:19:25 -0400

    angular front end added to JSFCarLot

---
+++ JSFCarLot/src/com/eintern/beans/CarLotFormBean.java
@@ -0,0 +1,14 @@
+package com.eintern.beans;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@RequestScoped
+public class CarLotFormBean {
+
+}
